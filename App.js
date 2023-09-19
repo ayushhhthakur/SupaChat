edc05867a96e99a2b0343f5d97d6221a403be10f
Ayush Thakur <ayushthakur1412@gmail.com>
2023-09-19 20:39:12 +0530

    Added Register Function

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Register from './pages/Register';
 
 const Stack = createStackNavigator();
 
@@ -10,8 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Udaan" component={Login} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Register" component={Register} />
         {/* Add other screens here */}
       </Stack.Navigator>
     </NavigationContainer>
