75320b312e22ce3afd26e309562750bd223b3035
Ayush Thakur <ayushthakur1412@gmail.com>
2023-09-19 20:39:58 +0530

    Updated Login Page

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Alert, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 function Login() {
@@ -10,54 +10,67 @@ function Login() {
 
   const handleLogin = () => {
     if (!username || !password) {
-      // Check if either the username or password is empty
       Alert.alert('Warning', 'Please enter both username and password.');
-      alert('Warning, Please enter both username and password.');
-      console.log("NOPE")
     } else if (username === 'user' && password === 'abc@123') {
-      // Redirect user to "Home" screen
       navigation.navigate('Home');
     } else {
-      // Display an error message
       Alert.alert('Error', 'Wrong username or password. Please try again.');
-      // Reset the values of placeholders
       setUsername('');
       setPassword('');
     }
   };
 
+  const handleSignUp = () => {
+    // Navigate to the signup screen
+    navigation.navigate('Register');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        onChangeText={(text) => setUsername(text)}
-        value={username}
-        required
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry
-        value={password}
-        required
-      />
-      <Button title="Login" onPress={handleLogin} />
-    </View>
+    <ImageBackground
+      source={require('../assets/background-login.png')} // Replace with your image path
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Text style={styles.header}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+          value={password}
+        />
+        <Button title="Login" onPress={handleLogin} />
+
+        <Text style={styles.signupText}>
+          Don't have an account? 
+          <Text style={styles.signupLink} onPress={handleSignUp}> Register Here!</Text>
+        </Text>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch' for different image behaviors
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust opacity as needed
   },
   header: {
     fontSize: 24,
     marginBottom: 20,
+    color: 'white', // Text color on top of the background image
   },
   input: {
     width: '80%',
@@ -67,6 +80,16 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 10,
     paddingLeft: 10,
+    backgroundColor: 'white', // Input field background color
+    opacity: 0.8, // Adjust opacity as needed
+  },
+  signupText: {
+    marginTop: 20,
+    color: 'white', // Text color on top of the background image
+  },
+  signupLink: {
+    color: 'blue',
+    textDecorationLine: 'underline',
   },
 });
 
